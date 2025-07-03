e0bbc72bdc6f7aa65e88da824dcef03f10050987
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 17:51:09 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_63.ts
+++ solutions/0015-3sum/solution_63.ts
@@ -12,15 +12,5 @@
     
      for(int i = 0; i < n; i ++){
         for(int j = i + 1; j < n; j++){
-            for(int k = j + 1; k < n; k++){
-                if(nums[i] + nums[j] + nums[k] == 0){
-                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
-                    Collections.sort(triplet);
-                    resultSet.add(triplet);
-                }
-            } 
+            
         }
-     }
-
-     return new ArrayList<List<Integer>>(resultSet);   
-    }
