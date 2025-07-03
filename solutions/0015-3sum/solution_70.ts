364fb9311d9d51e898921ab40176fb420d91435f
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 17:58:22 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_70.ts
+++ solutions/0015-3sum/solution_70.ts
@@ -4,19 +4,11 @@
 # URL: https://leetcode.com/problems/3sum/
 # Date: 7/3/2025
 
-//     }
-// }
-
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-    
-    Set<List<Integer>> resultSet = new HashSet<>();
-    Map<Integer, Integer> map = new HashMap<>();
-
-    int n = nums.length;
-…        }
+        }
      }
 
      return new ArrayList<List<Integer>>(resultSet);   
     }
 }
+
+
