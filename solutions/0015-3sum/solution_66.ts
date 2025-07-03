666e618a975a205799f3c44bdcc03f9a81ea477f
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 17:54:04 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_66.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 66)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+    
+    for(int i = 0; i < n; i++){
+        map.put(nums[i], i);
+    }
+
+    
+     for(int i = 0; i < n; i ++){
+        
+        for(int j = i + 1; j < n; j++){
+            if()
