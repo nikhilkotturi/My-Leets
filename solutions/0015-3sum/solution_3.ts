6fc58b4f79cdc8a3ccc8f3fd30b5626654b7272b
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-02 23:50:10 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_3.ts
+++ solutions/0015-3sum/solution_3.ts
@@ -4,13 +4,5 @@
 # URL: https://leetcode.com/problems/3sum/submissions/1684574912/
 # Date: 7/2/2025
 
-// 1. Brute Force Solution - O(n^3) - giving me TLE 
-
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-    
-//     Set<List<Integer>> resultSet = new HashSet<>();
-//     int n = nums.length;
-//      for(int i = 0; i < n; i ++){
-//         for(int j = i + 1; j < n; j++){
-//             for(int k = j + 1; k < n; k++){
+//     }
+// }
