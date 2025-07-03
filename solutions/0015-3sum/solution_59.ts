944939b45acbc6c0bf644c93bbf4222abc743256
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 17:46:56 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_59.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 59)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+        
+    }
+
+    int n = nums.length;
+     for(int i = 0; i < n; i ++){
+        for(int j = i + 1; j < n; j++){
+            for(int k = j + 1; k < n; k++){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                    Collections.sort(triplet);
