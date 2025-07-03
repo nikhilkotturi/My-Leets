71c559d050100ba417d77facf89ec0904f8701b6
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 17:24:27 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_37.ts
+++ solutions/0015-3sum/solution_37.ts
@@ -1,7 +1,7 @@
 # 3sum (Solution 37)
 # Difficulty: Medium
 # Language: Typescript
-# URL: https://leetcode.com/problems/3sum/
+# URL: https://leetcode.com/problems/3sum/submissions/1684574912/
 # Date: 7/3/2025
 
 //     }
