e24317ab89f5672e1619542b1aaa4087cabf4284
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-02 23:49:32 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_2.ts
+++ solutions/0015-3sum/solution_2.ts
@@ -4,23 +4,23 @@
 # URL: https://leetcode.com/problems/3sum/submissions/1684574912/
 # Date: 7/2/2025
 
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-    
-    Set<List<Integer>> resultSet = new HashSet<>();
-    int n = nums.length;
-     for(int i = 0; i < n; i ++){
-        for(int j = i + 1; j < n; j++){
-            for(int k = j + 1; k < n; k++){
-                if(nums[i] + nums[j] + nums[k] == 0){
-                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
-                    Collections.sort(triplet);
-                    resultSet.add(triplet);
-                }
-            } 
-        }
-     }
+//             for(int k = j + 1; k < n; k++){
+//      for(int i = 0; i < n; i ++){
+//         for(int j = i + 1; j < n; j++){
+//     Set<List<Integer>> resultSet = new HashSet<>();
+//     int n = nums.length;
+//     public List<List<Integer>> threeSum(int[] nums) {
+    
+// class Solution {
+//                 if(nums[i] + nums[j] + nums[k] == 0){
+//                     List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+//                     Collections.sort(triplet);
+//                     resultSet.add(triplet);
+//                 }
+//             } 
+//         }
+//      }
 
-     return new ArrayList<List<Integer>>(resultSet);   
-    }
-}
+//      return new ArrayList<List<Integer>>(resultSet);   
+//     }
+// }
