03665b1e5a0b310e7bc067caf3d6612aeca2450b
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 18:44:43 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_101.ts
@@ -0,0 +1,14 @@
+# 3sum (Solution 101)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+        }
+     }
+
+     return new ArrayList<List<Integer>>(resultSet);   
+    }
+}
+
+
