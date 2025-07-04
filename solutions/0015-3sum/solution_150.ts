da0cf736a8d775ea15b32fb936c98f3c21574937
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:38:59 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_150.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 150)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+        int j = i + 1;
+        int k = n - 1;
+
+        while(j < k){
+            sum = nums[i]
+        }
+     }
+
+     return new ArrayList<List<Integer>>(resultSet);   
+    }
