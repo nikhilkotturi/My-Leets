f4ae8185bbb2b49fc55c5efffa1145fac768a771
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 20:29:35 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_108.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 108)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+        Set<Integer> newSet = new HashSet<>();
+        for(int j = i + 1; j < n; j++){
+            int k = nums
+            if()
+        }
+     }
+
+     return new ArrayList<List<Integer>>(resultSet);   
+    }
+}
