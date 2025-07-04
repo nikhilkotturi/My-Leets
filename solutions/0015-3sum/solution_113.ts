e1d62012eee3f5638edc5f18a327330786109b52
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 22:41:08 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_113.ts
+++ solutions/0015-3sum/solution_113.ts
@@ -7,7 +7,7 @@
 //     }
 // }
 
-// 2. Somewhat better Solution - O(n^2) - two sum application
+// 2. Somewhat better Solution - O(n^2) - two sum application, but
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
