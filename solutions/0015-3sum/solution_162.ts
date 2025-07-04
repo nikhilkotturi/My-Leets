afab5a4fbedf28c13174165c485e1be4b482bcc4
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:52:10 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_162.ts
+++ solutions/0015-3sum/solution_162.ts
@@ -4,7 +4,7 @@
 # URL: https://leetcode.com/problems/3sum/
 # Date: 7/3/2025
 
-                while(j < k && nums[j] == nums[j - 1]) j++;
+                while(j < k && nums[j] == nums[j - 1]) j+;
             }
         }
      }
