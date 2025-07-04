076b54d94cb0a348a416cffe89cdd5ebfdd0dd56
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 22:40:33 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_112.ts
+++ solutions/0015-3sum/solution_112.ts
@@ -7,10 +7,10 @@
 //     }
 // }
 
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
     
     Set<List<Integer>> resultSet = new HashSet<>();
     int n = nums.length;
-
-     for(int i = 0; i < n; i ++){
