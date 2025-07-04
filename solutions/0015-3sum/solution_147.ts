2517c933d2fbdf4cf708cb818f81aa5c8bab2a27
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:36:41 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_147.ts
+++ solutions/0015-3sum/solution_147.ts
@@ -13,4 +13,4 @@ class Solution {
     int n = nums.length;
 
      for(int i = 0; i < n; i ++){
-        if(i > 0 && nums[i] == nums[i - 1]) continue
+        if(i > 0 && nums[i] == nums[i - 1]) continue;
