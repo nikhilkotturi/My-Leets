26eca409fd34d0cce06d5a374e1ad32bcef632aa
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 22:40:21 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_112.ts
+++ solutions/0015-3sum/solution_112.ts
@@ -1,16 +1,15 @@
 # 3sum (Solution 112)
 # Difficulty: Medium
 # Language: Typescript
-# URL: https://leetcode.com/problems/3sum/submissions/1685665231/
+# URL: https://leetcode.com/problems/3sum/
 # Date: 7/3/2025
 
-        Set<Integer> newSet = new HashSet<>();
-        for(int j = i + 1; j < n; j++){
-            int k = (-1)*nums[i] + (-1)*nums[j];
-            if(newSet.contains(k)){
-                List<Integer> triplet = Arrays.asList(nums[i], nums[j], k);
-                Collections.sort(triplet);
-                resultSet.add(triplet);   
             }
-            else{
-                newSet.add(nums[j]);
+
+        }
+     }
+
+     return new ArrayList<List<Integer>>(resultSet);   
+    }
+}
+
