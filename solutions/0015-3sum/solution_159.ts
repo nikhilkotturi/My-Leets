7a261fa2cb2c10e5be7c8148ac11a15b66b8d043
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:49:06 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_159.ts
+++ solutions/0015-3sum/solution_159.ts
@@ -4,13 +4,13 @@
 # URL: https://leetcode.com/problems/3sum/
 # Date: 7/3/2025
 
-//                 if(nums[i] + nums[j] + nums[k] == 0){
-//                     List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
-//                     Collections.sort(triplet);
-//                     resultSet.add(triplet);
-//                 }
-//             } 
-//         }
-//      }
 
-//      return new ArrayList<List<Integer>>(resultSet);   
+            }
+
+            else{
+                
+            }
+        }
+     }
+
+     return new ArrayList<List<Integer>>(resultSet);   
