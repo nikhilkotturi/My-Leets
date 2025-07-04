38a006d0a91a055ae2af4a95ac6332d4211a7fed
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:51:11 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_162.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 162)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+
+            }
+
+            else{
+                List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                
+            }
+        }
+     }
+
