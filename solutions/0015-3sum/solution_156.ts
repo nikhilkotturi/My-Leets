c3c4d4f8d7860d13d143941fed1cf8bf15074afe
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:45:05 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_156.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 156)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/
+# Date: 7/3/2025
+
+        int j = i + 1;
+        int k = n - 1;
+
+        while(j < k){
+            sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0){
+
+            }
+
+            else if(sum > 0){
