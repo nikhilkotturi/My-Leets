a5a90a7645a0f3ab587ce4ab7c752e6053488df5
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-04 09:15:02 -0500

    Add 3sum solution

---
+++ solutions/0015-3sum/solution_176.ts
@@ -0,0 +1,16 @@
+# 3sum (Solution 176)
+# Difficulty: Medium
+# Language: Typescript
+# URL: https://leetcode.com/problems/3sum/submissions/1685739515/
+# Date: 7/4/2025
+
+// }
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+    
+    // Set<List<Integer>> resultSet = new HashSet<>();
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    Arrays.sort(nums);
