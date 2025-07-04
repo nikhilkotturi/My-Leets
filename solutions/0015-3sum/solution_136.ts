b0b22d789a9f558a4062c953d63fbcb171aba8ae
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-03 23:25:15 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_136.ts
+++ solutions/0015-3sum/solution_136.ts
@@ -4,15 +4,5 @@
 # URL: https://leetcode.com/problems/3sum/
 # Date: 7/3/2025
 
-//     }
 // }
-
-// 2. Somewhat better Solution - O(n^2) - two sum application, but using two loops
-
-// class Solution {
-//     public List<List<Integer>> threeSum(int[] nums) {
-    
-//     Set<List<Integer>> resultSet = new HashSet<>();
-//     int n = nums.length;
-…// }
 
