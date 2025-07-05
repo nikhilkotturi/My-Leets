b6522befa6cf6595f07a666e0644be5bf34f1909
Nikhil Padmanabh Kotturi <121041171+nikhilkotturi@users.noreply.github.com>
2025-07-04 22:54:25 -0500

    Add 3sum solution

--- solutions/0015-3sum/solution_281.ts
+++ solutions/0015-3sum/solution_281.ts
@@ -4,13 +4,4 @@
 # URL: https://leetcode.com/problems/3sum/
 # Date: 7/4/2025
 
-
-     for(int i = 0; i < n; i ++){
-        if(i > 0 && nums[i] == nums[i - 1]) continue;
-        int j = i + 1;
-        int k = n - 1;
-
-        while(j < k){
-          int sum = nums[i] + nums[j] + nums[k];
-            if(sum < 0){
-                j++;
+}
